9a1fff216b837ef20fc98daf4cd3577719daad18
Yann Chemin <dr.yann.chemin@gmail.com>
2025-12-14 21:43:37 +0100

    update

--- i.hyper.smac.py
+++ i.hyper.smac.py
@@ -389,9 +389,11 @@ def apply_smac_correction_simple(input_raster, output_raster, bands,
             band_num = band['band_num']
             temp_band = f"temp_band_{band_num}_{os.getpid()}"
             temp_band_corr = f"{temp_band}_corr"
-            
+            # Add a small epsilon to prevent division by zero
+            epsilon = 1e-10
+            expr = f"{temp_band_corr} = float(min(max(0.0, ({temp_band} - {rho_atm}) / max({t_total}, {epsilon})), 1.0))"
             # Apply the correction
-            expr = f"{temp_band_corr} = ({temp_band} - {corr['rho_atm']:.6f}) / {corr['t_total']:.6f}"
+            #expr = f"{temp_band_corr} = ({temp_band} - {corr['rho_atm']:.6f}) / {corr['t_total']:.6f}"
             gs.run_command('r.mapcalc', expression=expr, overwrite=True, quiet=True)
 
             # Add wavelength and FWHM to the output band
